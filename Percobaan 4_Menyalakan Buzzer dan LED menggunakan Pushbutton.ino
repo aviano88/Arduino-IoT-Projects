7ae34d9352c5136cb7fc09d65f9ca9e271c277b0
aviano88 <67731935+aviano88@users.noreply.github.com>
2023-01-25 15:11:02 +0700

    Create Percobaan 4_Menyalakan Buzzer dan LED menggunakan Pushbutton.ino
    // Project 4 : Turn On Buzzer and LED using pushbutton

---
+++ Percobaan 4_Menyalakan Buzzer dan LED menggunakan Pushbutton.ino
@@ -0,0 +1,35 @@
+// Project 4 : Turn On Buzzer and LED using pushbutton 
+// constants won't change. They're used here to set pin numbers:
+const int buttonPin = 2;  // the number of the pushbutton pin
+const int ledPin = 13;    // the number of the LED pin
+      int buzzerPin = 9;
+
+// variables will change:
+int buttonState = 0;  // variable for reading the pushbutton status
+
+void setup() {
+  // initialize the LED pin as an output:
+  pinMode(ledPin, OUTPUT);
+  pinMode(buzzerPin, OUTPUT);
+  // initialize the pushbutton pin as an input:
+  pinMode(buttonPin, INPUT);
+  Serial.begin(9600);
+}
+
+void loop() {
+  // read the state of the pushbutton value:
+  buttonState = digitalRead(buttonPin);
+
+  // check if the pushbutton is pressed. If it is, the buttonState is HIGH:
+  if (buttonState == HIGH) {
+    // turn LED on:
+    digitalWrite(ledPin, HIGH);
+    tone(buzzerPin,1000,5000);
+    delay(500);
+  } else {
+    // turn LED off:
+    digitalWrite(ledPin, LOW);
+    noTone(buzzerPin);
+    delay(500);
+  }
+}
